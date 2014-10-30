b984b12cd8ee0671364049b4cf4551858c643001
outzider <dgethers.jr@gmail.com>
2014-10-30 19:15:45 -0400

    Initial Revision.

---
+++ UILabs/src/course/labs/todomanager/ToDoListAdapter.java
@@ -0,0 +1,144 @@
+package course.labs.todomanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+
+public class ToDoListAdapter extends BaseAdapter {
+
+	private final List<ToDoItem> mItems = new ArrayList<ToDoItem>();
+    private final LayoutInflater inflater;
+
+	private static final String TAG = "Lab-UserInterface";
+
+	public ToDoListAdapter(Context context) {
+
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	// Add a ToDoItem to the adapter
+	// Notify observers that the data set has changed
+
+	public void add(ToDoItem item) {
+
+		mItems.add(item);
+		notifyDataSetChanged();
+
+	}
+
+	// Clears the list adapter of all items.
+
+	public void clear() {
+
+		mItems.clear();
+		notifyDataSetChanged();
+
+	}
+
+	// Returns the number of ToDoItems
+
+	@Override
+	public int getCount() {
+
+		return mItems.size();
+
+	}
+
+	// Retrieve the number of ToDoItems
+
+	@Override
+	public Object getItem(int pos) {
+
+		return mItems.get(pos);
+
+	}
+
+	// Get the ID for the ToDoItem
+	// In this case it's just the position
+
+	@Override
+	public long getItemId(int pos) {
+
+		return pos;
+
+	}
+
+	// Create a View for the ToDoItem at specified position
+	// Remember to check whether convertView holds an already allocated View
+	// before created a new View.
+	// Consider using the ViewHolder pattern to make scrolling more efficient
+	// See: http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+	private static class ViewHolder {
+        TextView titleView;
+        CheckBox statusView;
+        TextView priorityView;
+        TextView dateView;
+    }
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+		if (convertView == null) {
+
+            convertView = inflater.inflate(R.layout.todo_item, parent, false);
+            holder = new ViewHolder();
+            holder.titleView = (TextView) convertView.findViewById(R.id.titleView);
+            holder.statusView = (CheckBox) convertView.findViewById(R.id.statusCheckBox);
+            holder.priorityView = (TextView) convertView.findViewById(R.id.priorityView);
+            holder.dateView = (TextView) convertView.findViewById(R.id.dateView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        final ToDoItem toDoItem = mItems.get(position);
+
+        holder.titleView.setText(toDoItem.getTitle());
+
+        final CheckBox statusView = holder.statusView;
+        ToDoItem.Status status = toDoItem.getStatus();
+        if (status == ToDoItem.Status.DONE) {
+            holder.statusView.setChecked(true);
+        } else {
+            holder.statusView.setChecked(false);
+        }
+
+        statusView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                Log.i(TAG, "Entered onCheckedChanged()");
+
+                if (statusView.isChecked()) {
+                    toDoItem.setStatus(ToDoItem.Status.DONE);
+                } else {
+                    toDoItem.setStatus(ToDoItem.Status.NOTDONE);
+                }
+            }
+        });
+
+        holder.priorityView.setText(toDoItem.getPriority().toString());
+
+        holder.dateView.setText(ToDoItem.FORMAT.format(toDoItem.getDate()));
+
+		// Return the View you just created
+		return convertView;
+
+	}
+}
